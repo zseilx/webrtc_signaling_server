3777b0cec2cd9d9ab7f0307d1ea4157dd2a9582b
Jung Younghwa <zseil@naver.com>
2018-06-05 23:12:27 +0900

    2018_06_05_01 Updating

--- server.js
+++ server.js
@@ -5,5 +5,28 @@
  *       
  */
 
+"use strict";
 
- 
+var config = require("./config.json");
+var http = require("http");
+//var socket = require("socket.io");
+var ws = require('ws');
+
+
+var httpServer = null;
+if (config.httpConfig) {
+    httpServer = http.createServer();
+    httpServer.listen(
+        config.httpConfig.port 
+        ,function () { console.log('Listening on ' + httpServer.address().port); }
+    );
+}
+
+
+var websocketSignalingServer = new wns.WebsocketNetworkServer()
+if (httpServer) {
+    //perMessageDeflate: false needs to be set to false turning off the compression. if set to true
+    //the websocket library crashes if big messages are received (eg.128mb) no matter which payload is set!!!
+    var webSocket = new ws.Server({ server: httpServer, path: config.app.path, maxPayload: config.maxPayload, perMessageDeflate: false });
+    websocketSignalingServer.addSocketServer(webSocket, config.app);
+}
